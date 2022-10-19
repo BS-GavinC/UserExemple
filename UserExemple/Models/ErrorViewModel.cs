400891b3d01e405ecb8d9cfc4ce250a94aaea511
VZGLY <chaineux.gavin@gmail.com>
2022-10-19 16:59:48 +0200

    Ajoutez des fichiers projet.

---
+++ UserExemple/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserExemple.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
